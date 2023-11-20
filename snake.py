f36ec39128f730d7dd2d7488e4b2c2e19bb4a20a
solalcaraz <137455954+solalcaraz@users.noreply.github.com>
2023-11-20 02:31:54 -0300

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,20 +1,11 @@
-'''
-Juego de Snake.
-Participantes: Maria Sol Alcaraz
-			  Franco Medina
-			  Damian Palomba
-'''
-
-import pygame, sys, random
+import pygame
+from random import randint
 
 #Pantalla
-#Cuadrados de 20x20, 25 de alto, 40 de ancho
-ANCHO = 800 
-ALTO = 500
-CUADROS = 20
-#Dimensiones de la ventana (tupla)
-SCREEN_ANCHO = 800
-SCREEN_ALTO = 500
+ANCHO = 800
+ALTURA = 600 
+#CADA BLOQUE ES DE 20x20
+
 #FPS
 FPS = 30
 #Paleta de colores
@@ -22,165 +13,127 @@ NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
 COLOR_CUERPO = (0, 255, 255)
 COLOR_FRUTA = (255, 200, 0)
-#Direcciones
-IZQUIERDA = (-1, 0)
-DERECHA = (1, 0)
-ARRIBA =(0, -1)
-ABAJO = (0, 1)
-#Preferencias
+
 LARGO_MAX = 10
-FPS = 60
-
-#Preferencias del snake
-class Snake(pygame.sprite.Sprite):
-	def __init__(self):
-		#Caracteristicas visuales del snake
-		self.imagen_izq = pygame.image.load("cabeza_izquierda.png").convert()
-		self.imagen_izq.set_colorkey(BLANCO)
-		self.imagen_der = pygame.image.load("cabeza_derecha.png").convert()
-		self.imagen_der.set_colorkey(BLANCO)
-		self.imagen_up = pygame.image.load("cabeza_arriba.png").convert()
-		self.imagen_up.set_colorkey(BLANCO)
-		self.imagen_down = pygame.image.load("cabeza_abajo.png").convert()
-		self.imagen_down.set_colorkey(BLANCO)
-
-		self.rect_izq = self.imagen_izq.get_rect()
-		self.rect_der = self.imagen_der.get_rect()
-		self.rect_up = self.imagen_up.get_rect()
-		self.rect_down = self.imagen_down.get_rect()
-		
-		self.largo = 1
-		self.posicion = [(ANCHO // 2, ALTO // 2)]
-		self.direccion = DERECHA
-		self.velocidad = 2
-
-	def posicion_inicial(self):
-		'''Devuelve la posicion de la cabeza'''
-		return self.posicion[0]
-
-	def es_perdedor(self):
-		'''Devuelve True si la cabeza de la serpiente está por fuera de los confines del campo.'''
-		posicion_cabeza = self.posicion_inicial()
-		return (
-        	(posicion_cabeza[0] >= ALTO or posicion_cabeza[0] < 0) or
-        	(posicion_cabeza[1] >= ANCHO or posicion_cabeza[1] < 0) or
-        	(self.posicion.count(posicion_cabeza) > 1)
-    	)
+VELOCIDAD = 15
 
 
-	def es_ganador(self):
-		'''Devuelve True si la longitud de la serpiente alcanza el largo máximo permitido.'''
-		return self.largo == LARGO_MAX
-
-	def reset(self):
-		self.largo = 1
-		self.posicion = [POS_INICIAL]
-		self.direccion = DERECHA
-		self.velocidad = 1
-
-	def dibujo(self, pantalla):
-		cabeza = self.posicion_inicial()
-		if self.direccion == IZQUIERDA:
-			pantalla.blit(self.imagen_izq, cabeza)
-		elif self.direccion == DERECHA:
-			pantalla.blit(self.imagen_der, cabeza)
-		elif self.direccion == ARRIBA:
-			pantalla.blit(self.imagen_up, cabeza)
-		elif self.direccion == ABAJO:
-			pantalla.blit(self.imagen_down, cabeza)
-
-		for segmento in self.posicion[1:]:
-			pygame.draw.rect(pantalla, COLOR_CUERPO, (segmento[0], segmento[1], CUADROS, CUADROS))
-
-	def update(self):
-		#Actualiza la posicion en funcion de la direccion
-		x, y = self.posicion[0]
-		new_x = x + self.direccion[0] * self.velocidad
-		new_y = y + self.direccion[1] * self.velocidad
-		self.posicion.insert(0, (new_x, new_y))
-
-	def cambiar_direccion(self, nueva_direccion):
-		#Cambia la direccion del snake
-		if (self.direccion[0] + nueva_direccion[0]!= 0 or self.direccion[1] + nueva_direccion[1] != 0):
-			self.direccion = nueva_direccion
-
-	def comio_fruta(self, fruta):
-		cabeza_x, cabeza_y = self.posicion_inicial()
-		fruta_x, fruta_y = fruta.posicion
-		cabeza_rect = pygame.Rect(cabeza_x, cabeza_y, CUADROS, CUADROS)
-		fruta_rect = pygame.Rect(fruta_x, fruta_y, CUADROS, CUADROS)
-		return cabeza_rect.colliderect(fruta_rect)
-
-#Preferencias de la fruta
-class Fruta(pygame.sprite.Sprite):
-	def __init__(self,):
-		self.imagen_fruta = pygame.image.load("manzana.png").convert()
-		self.imagen_fruta.set_colorkey(BLANCO)
-		self.rect_fruta = self.imagen_fruta.get_rect()
-		self.posicion = (0, 0)
-		self.random_posicion()
-
-	def random_posicion(self):
-		self.posicion = (random.randint(0, ALTO - 1), random.randint(0, ANCHO - 1))
-
-	def actualizar_fruta(self, snake):
-		if snake.posicion_inicial() == self.posicion:
-			self.random_posicion()
-
-	def dibujo_fruta(self, pantalla):
-		pantalla.blit(self.imagen_fruta, self.posicion)
-
-'''Comienzo del juego.
-Inicializacion de pygame, configuracion general y bucle principal.
-'''
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((ANCHO, ALTO))
-	pygame.display.set_caption('Juego Snake')
-	clock = pygame.time.Clock()
-	fondo = pygame.image.load("fondo.png").convert()
-	iniciado = False
+    pygame.init()
+    pygame.display.set_caption('Juego Snake')
+    ventana = pygame.display.set_mode((ANCHO, ALTURA))
+    fps = pygame.time.Clock()
+    fondo = pygame.image.load("fondo.png").convert()
+    
+    snake = [[400,300],[380,300],[360,300],[340,300]]
+    pos_snake = [340,300] #Posicion de la cabeza del snake en todo momento.
+    fruta = posicion_aleatoria(ANCHO,ALTURA,snake);
+    
+    cabeza_izq = pygame.image.load("cabeza_izquierda.png").convert()
+    cabeza_izq.set_colorkey(NEGRO)
+    cabeza_der = pygame.image.load("cabeza_derecha.png").convert()
+    cabeza_der.set_colorkey(NEGRO)
+    cabeza_up = pygame.image.load("cabeza_arriba.png").convert()
+    cabeza_up.set_colorkey(NEGRO)
+    cabeza_abj = pygame.image.load("cabeza_abajo.png").convert()
+    cabeza_abj.set_colorkey(NEGRO)
 
-	snake = Snake()
-	fruta = Fruta()
+    cuerpo_imag = pygame.image.load("cuerpo.png").convert()
+    cuerpo_imag.set_colorkey(NEGRO)
 
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
-			elif event.type == pygame.KEYDOWN:
-				if not iniciado:
-					iniciado = True
-				if event.key == pygame.K_UP:
-					snake.cambiar_direccion(ARRIBA)
-				elif event.key == pygame.K_DOWN:
-					snake.cambiar_direccion(ABAJO)
-				elif event.key == pygame.K_LEFT:
-					snake.cambiar_direccion(IZQUIERDA)
-				elif event.key == pygame.K_RIGHT:
-					snake.cambiar_direccion(DERECHA)
+    fruta_imag = pygame.image.load("manzana.png").convert()
+    fruta_imag.set_colorkey(NEGRO)
 
-		if iniciado:
-			snake.update()
-			fruta.actualizar_fruta(snake)
-			if snake.comio_fruta(fruta):
-				snake.largo += 1
-				fruta.random_posicion()
+    direccion = 'RIGHT'
+    cabeza_imagen = cabeza_der
+    dir_siguiente = direccion
 
-		if snake.es_perdedor() or snake.es_ganador():
-			snake.reset()
-			fruta.random_posicion()
+    while(True):
+        direccion, dir_siguiente, cabeza_imagen = mover_snake(direccion, dir_siguiente, cabeza_imagen, cabeza_der, cabeza_abj, cabeza_up, cabeza_izq, pos_snake)
+        snake.append(pos_snake.copy())
+        if pos_snake == fruta:
+            fruta = posicion_aleatoria(ANCHO,ALTURA,snake);
+        else:
+            snake = snake[1:]
 
-		screen.blit(fondo, [0, 0])
+        ventana.blit(fondo, [0, 0])
 
-		fruta.dibujo_fruta(screen)
-		snake.dibujo(screen)
+        imprimir_campo(snake, fruta, ventana, cabeza_imagen, cuerpo_imag, fruta_imag, pos_snake)
+        if es_perdedor(snake, pos_snake): 
+            game_over(ventana)
+        pygame.display.update()
+        fps.tick(VELOCIDAD)
+    return
 
-		pygame.display.update()
-		clock.tick(FPS)
-
-	pygame.quit()
-
-if __name__ == "__main__":
-    main()
+def mover_snake(direccion, dir_siguiente, cabeza_imagen, cabeza_der, cabeza_abj, cabeza_up, cabeza_izq, pos_snake):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                dir_siguiente = 'UP'
+            if event.key == pygame.K_DOWN:
+                dir_siguiente = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                dir_siguiente = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                dir_siguiente = 'RIGHT'
+ 
+    # Ocupa el caso en el que se presionen dos teclas opuestas a la vez.
+    if dir_siguiente == 'UP' and direccion != 'DOWN':
+        direccion = 'UP'
+    if dir_siguiente == 'DOWN' and direccion != 'UP':
+        direccion = 'DOWN'
+    if dir_siguiente == 'LEFT' and direccion != 'RIGHT':
+        direccion = 'LEFT'
+    if dir_siguiente == 'RIGHT' and direccion != 'LEFT':
+        direccion = 'RIGHT'
+ 
+    # Mueve el snake un bloque
+    if direccion == 'UP':
+        pos_snake[1] -= 20
+        cabeza_imagen = cabeza_up
+    if direccion == 'DOWN':
+        pos_snake[1] += 20
+        cabeza_imagen = cabeza_abj
+    if direccion == 'LEFT':
+        pos_snake[0] -= 20
+        cabeza_imagen = cabeza_izq
+    if direccion == 'RIGHT':
+        pos_snake[0] += 20
+        cabeza_imagen = cabeza_der
+        
+    return [direccion, dir_siguiente, cabeza_imagen]
+    
+def posicion_aleatoria(ancho, altura, snake):
+    '''Recibe las dimensiones de un campo de ALTURAxANCHO, y la lista de posiciones del snake.
+       Devuelve una posición [x,y] vacía aleatoria dentro de esas dimensiones.
+       En este caso, "vacía" significa que la posición no se encuentra ocupada por el snake.'''
+    [x, y] = [randint(1, (ANCHO-20)//20)*20, randint(1, (ALTURA-20)//20)*20]
+    while [x, y] in snake:
+        [x, y] = [randint(1, (ANCHO-20)//20)*20, randint(1, (ALTURA-20)//20)*20]
+    return [x, y]
+    
+def imprimir_campo(snake, fruta, ventana, cabeza_imagen, cuerpo_imag, fruta_imag, pos_snake):
+    #ventana.blit(cabeza_imagen, pos_snake)
+    for i, posicion in enumerate(snake[1:]):
+        #if i == 0: #Cabeza del snake
+        #    ventana.blit(cabeza_imagen, pos_snake)
+        #else: #Cuerpo general
+        ventana.blit(cuerpo_imag, (posicion[0], posicion[1]))
+        #pygame.draw.rect(ventana, COLOR_CUERPO, pygame.Rect(posicion[0],posicion[1],20,20))
+    ventana.blit(cabeza_imagen, pos_snake)
+    ventana.blit(fruta_imag, (fruta[0], fruta[1]))
+    
+def es_perdedor(snake, pos_snake):
+    #Busco si la proxima posicion esta dentro del snake, si lo está la cabeza tocó el cuerpo.
+    if any(pos_snake == pos_cuerpo for pos_cuerpo in snake[:-1]):
+        return True
+    #Chequeo que la proxima posicion no se salga del campo.
+    if (pos_snake[0] < 0 or pos_snake[0] > ANCHO - 20 or pos_snake[1] < 0 or pos_snake[1] > ALTURA - 20):
+        return True
+        
+def game_over(ventana):
+    pygame.draw.rect(ventana, BLANCO, pygame.Rect(400,300,20,20))
+    
+main()
